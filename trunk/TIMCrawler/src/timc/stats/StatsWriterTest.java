08a6ddd9604888c277124e9891776a0d3d179b64
asafshmir <asafshmir@99d24f90-0284-dfdf-c06c-bb8da34ac033>
2012-05-18 15:30:55 +0000

    changed StatsWriter methods to work with TestRecord and SessionRecord added methods writeTrackerSessionStats and updateTestStats

--- trunk/TIMCrawler/src/timc/stats/StatsWriterTest.java
+++ trunk/TIMCrawler/src/timc/stats/StatsWriterTest.java
@@ -17,49 +17,78 @@ public class StatsWriterTest {
 	public static void main(String[] args) {
 
 		DOMConfigurator.configure("config/log4j.xml");
-		
+
 		try {
 			String driver = "com.mysql.jdbc.Driver";
 			Class.forName(driver).newInstance();
-		}
-		catch(Exception x){
+		} catch (Exception x) {
 			System.out.println("Unable to load the driver class!");
 		}
-		
-		
+
 		StatsWriter sw = new DBStatsWriter();
-		
+
 		sw.initWriter();
-		
+
 		Date start = new Date();
 		try {
 			Thread.sleep(1000);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-		
-		byte[] peerId = {'-','A','Z','1','0','0','3','-'};
+
+		byte[] peerId = { '-', 'A', 'Z', '1', '0', '0', '3', '-' };
 		Peer peer1 = new Peer("192.168.0.111", 6882, ByteBuffer.wrap(peerId));
 		Peer peer2 = new Peer("192.168.0.222", 6885, ByteBuffer.wrap(peerId));
-		Peer peer3 = new Peer("192.168.0.333", 6886, ByteBuffer.wrap(peerId));
+
 		String infoHash = "just_a_hash";
 		Date lastSeen = new Date();
-		
-		
-		Object id = sw.writeTestStats(start, new Date(), infoHash, 1024, 512, 2);
-		
+
+		TestRecord tr = new TestRecord();
+		tr.mode = 0;
+		tr.startTime = start;
+		tr.infoHash = infoHash;
+		tr.totalSize = 1024;
+		tr.pieceSize = 512;
+		tr.numPieces = 2;
+		Object testId = sw.writeTestStats(tr);
+
 		try {
 			Thread.sleep(1000);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-		
-		sw.writeSessionStats(id, peer1, start, lastSeen, null);
-		sw.writeSessionStats(id, peer2, start, lastSeen, null);
-		sw.writeSessionStats(id, peer3, start, lastSeen, null);
-		
+
+		SessionRecord sr1 = new SessionRecord();
+		sr1.peerID = peer1.getPeerIdStr();
+		sr1.peerIP = peer1.getIp();
+		sr1.peerPort = peer1.getPort();
+		sr1.startTime = start;
+		sr1.lastSeen = lastSeen;
+		sr1.crawlerPeerID = "-UT2002-";
+
+		SessionRecord sr2 = new SessionRecord();
+		sr2.peerID = peer2.getPeerIdStr();
+		sr2.peerIP = peer2.getIp();
+		sr2.peerPort = peer2.getPort();
+		sr2.startTime = start;
+		sr2.lastSeen = lastSeen;
+		sr2.crawlerPeerID = "-UT2002-";
+
+		sw.writeSessionStats(testId, sr1);
+		sw.writeSessionStats(testId, sr2);
+
+		sr1.lastSeenByTracker = new Date();
+		sw.writeTrackerSessionStats(testId, sr1);
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		sr1.lastSeenByTracker = new Date();
+		sw.writeTrackerSessionStats(testId, sr1);
+
+		tr.endTime = new Date();
+		sw.updateTestStats(testId, tr);
 		sw.closeWriter();
-
 	}
-
 }
---
+++ trunk/TIMCrawler/src/timc/stats/TestRecord.java
@@ -0,0 +1,16 @@
+package timc.stats;
+
+import java.util.Date;
+
+public class TestRecord {
+	
+	public int mode;
+	public String modeSettings;
+	public Date startTime;
+	public Date endTime;
+	public String infoHash;
+	public int totalSize;
+	public int pieceSize;
+	public int numPieces;
+
+}
