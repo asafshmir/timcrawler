e1118a72b73d07adbee375eda2674ec00e7c62c0
shwager.omer@gmail.com <shwager.omer@gmail.com@99d24f90-0284-dfdf-c06c-bb8da34ac033>
2012-05-22 13:55:09 +0000

    updated StatsLogger changed SessionRecord types

--- trunk/TIMCrawler/src/timc/stats/logger/StatsLogger.java
+++ trunk/TIMCrawler/src/timc/stats/logger/StatsLogger.java
@@ -1,10 +1,12 @@
 package timc.stats.logger;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.nio.ByteBuffer;
+import java.text.ParseException;
 import java.util.BitSet;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -15,68 +17,120 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import timc.stats.SessionRecord;
+import timc.stats.TestRecord;
 import timc.stats.StatsWriter;
 import timc.stats.db.DBStatsWriter;
+import timc.stats.PeerStats;
 
 import com.turn.ttorrent.bcodec.BEValue;
+import com.turn.ttorrent.bcodec.InvalidBEncodingException;
 import com.turn.ttorrent.client.Announce;
 import com.turn.ttorrent.client.AnnounceResponseListener;
 import com.turn.ttorrent.client.Piece;
 import com.turn.ttorrent.client.SharedTorrent;
 import com.turn.ttorrent.client.peer.PeerActivityListener;
 import com.turn.ttorrent.client.peer.SharingPeer;
+import com.turn.ttorrent.common.Peer;
+import com.turn.ttorrent.common.Torrent;
 
 import java.util.Date;
 
-public class StatsLogger implements Runnable, 
-	AnnounceResponseListener, PeerActivityListener {
-	
-	// TODO: add getNumSeeders and getNumLeechers methods to SharedTorrent or Announce
+public class StatsLogger implements AnnounceResponseListener, PeerActivityListener {		
+	// TODO add logging for diagnostics
 	
 	private static final Logger logger =
 			LoggerFactory.getLogger(StatsLogger.class);
 	
-	private boolean stop; // TODO - need to enable stopping from Client
-	private ConcurrentMap<String, SharingPeer> connectedPeers;  // same instance as Client's "connected"
-	private ConcurrentMap<String, Map<Integer, SessionRecord>> sessionsMap;
+	private ConcurrentMap<String, SharingPeer> connectedPeers;  // same instance as Client's "connected"	
+	private ConcurrentMap<String, PeerStats> sessionsMap;
 	private String crawlerPeerID;
 	private SharedTorrent torrent;
-	private int sleepIntervalMiliSecs;
 	private Announce announce;
-	private Thread thread;
 	private StatsWriter statsWriter;
-	
-	// peers and connected must not be null
+	private TestRecord testRecord;
+	private String testId;
+	private int numSeeds;
+	private int numLeeches;
+		
 	public StatsLogger(ConcurrentMap<String, SharingPeer> connected, SharedTorrent torrent, 
 			Announce announce, String crawlerPeerID)
 			throws IOException {
 		this.torrent = torrent;
 		this.connectedPeers = connected;
 		this.crawlerPeerID = crawlerPeerID;
-		this.thread = null;
 		this.announce = announce;
-		this.announce.register(this);
-		this.statsWriter = new DBStatsWriter();
-		this.statsWriter.initWriter();
-		
-		logger.info("StatsLogger [..{}] for {} started.",
-				new Object[] {
-					this.crawlerPeerID,
-					this.torrent.getName()
-				});					
+		this.announce.register(this);		
 	}
 	
-	public void start() {
-		this.stop = false;
-		if (this.thread == null || !this.thread.isAlive()) {
-			this.thread = new Thread(this);
-			this.thread.setName("bt-statsLogger");
-			this.thread.start();
+	public void start() {		
+		DOMConfigurator.configure("config/log4j.xml");
+		logger.info("Starting statsLogger for torrent" +
+				torrent.getName() + " with crawlerPeerID " +
+				this.crawlerPeerID + "...");
+				
+		this.statsWriter = new DBStatsWriter();		
+		this.statsWriter.initWriter();
+		this.sessionsMap = new ConcurrentHashMap<String, PeerStats>();	
+				
+		int numSeedsInTorrent = getNumSeedsInTorrent();
+		int numLeechInTorrent = getNumLeechInTorrent();
+		
+		// write the general test record, and get testId while doing so
+		this.testRecord = new TestRecord();
+		testRecord.mode = 0; // TODO: get from config file
+		testRecord.startTime = new Date();
+		testRecord.infoHash = this.torrent.getHexInfoHash();
+		testRecord.totalSize = this.torrent.getSize();
+		testRecord.pieceSize = Math.round(this.torrent.getSize() / this.torrent.getPieceCount());
+		testRecord.numPieces = this.torrent.getPieceCount();
+		
+		this.testId = this.statsWriter.writeTestStats(this.testRecord);	
+		logger.info("StatsLogger: writing to TestStats table, with testRecord: " + testRecord.toString());
+		
+		// init peer stats for peers that have already been connected
+		for (SharingPeer peer : this.connectedPeers.values()) {
+			if (!(sessionsMap.containsKey(peer.getHexPeerId())) ||  sessionsMap.get(peer.getHexPeerId()).getCurrentSessionNum() == 0 ){
+				PeerStats ps = new PeerStats();			
+				ps.setCurrentSessionNum(1);
+				SessionRecord rec1 = createNewRecord(numSeedsInTorrent,
+						numLeechInTorrent, peer, ps.getCurrentSessionNum());
+				ps.setCurrentSessionRecord(rec1);
+				ps.setConnected(true);	
+				ps.setSharingPeer(peer);
+				sessionsMap.put(peer.getHexPeerId(), ps);
+			}		
 		}
 	}
 	
 	public void stop() {
-		this.stop = true;
+		
+		for (PeerStats ps : sessionsMap.values()){
+			// write all zero records that have been initialized (thru announce responce handler
+			if (ps.getZeroSessionRecord() != null){
+				SessionRecord zeroRec = ps.getZeroSessionRecord();
+				try {
+					this.statsWriter.writeTrackerSessionStats(testId, zeroRec);	
+					logger.info("StatsLogger: stop method was invoked. Writing to Sessions table, for testID: "
+							+ testId + " with a Zero session record: " + zeroRec.toString());
+				} catch (Exception e) {
+					logger.error("StatsLogger error: {}", e.getMessage(), e);
+				}	
+			}			
+			
+			// write all last records of peers who haven't been marked as disconnected
+			if (ps.isConnected()){
+				SessionRecord currentRec =  ps.getCurrentSessionRecord();
+				updateRecordOnDisconnection(currentRec, ps.getSharingPeer(), true, getNumSeedsInTorrent(), getNumLeechInTorrent());
+				try {
+					this.statsWriter.writeSessionStats(testId, currentRec);	
+					logger.info("StatsLogger: stop method was invoked. Writing to Sessions table, for testID: " + testId 
+							+ " with session record: " + currentRec.toString());
+				} catch (Exception e) {
+					logger.error("{}", e.getMessage(), e);
+				}				
+			}			
+		}		
+		this.statsWriter.closeWriter();
 	}
 
 	@Override
@@ -90,105 +144,134 @@ public class StatsLogger implements Runnable,
 
 	@Override
 	public void handlePeerDisconnected(SharingPeer peer) {
-		// TODO - make sure that every time we disconnect from a peer this method is called, including all scenarios
-		
-		Map<Integer, SessionRecord> peerSessions = sessionsMap.get(peer.getHexPeerId());
-				
-		SessionRecord rec = peerSessions.get(peerSessions.size() - 1);
-		
-		updateRecordOnDisconnection(rec, peer);
-		
+		if (!sessionsMap.containsKey(peer.getHexPeerId()) || !sessionsMap.get(peer.getHexPeerId()).isConnected() || sessionsMap.get(peer.getHexPeerId()).getCurrentSessionNum() == 0){
+			logger.error("StatsLoger: error - peer with peerId: " + peer.getPeerId() + " disconnected, but we didn't have it as connected in sessionsMap, or didn't have it at all");
+			return;
+		}		
+		PeerStats ps = sessionsMap.get(peer.getHexPeerId());
+		SessionRecord currentRec =  ps.getCurrentSessionRecord();
+		updateRecordOnDisconnection(currentRec, peer, false, getNumSeedsInTorrent(), getNumLeechInTorrent());
+		ps.setConnected(false);
 		try {
-			logStatsRecord(rec);
-			
+			this.statsWriter.writeSessionStats(testId, currentRec);
+			logger.info("StatsLogger: the following peer has disconnected. Writing to Sessions table, for testID: " + testId 
+					+ " with session record: " + currentRec.toString());
 		} catch (Exception e) {
-			// TODO: handle exception
+			logger.error("StatsLogger error: {}", e.getMessage(), e);
 		}
-		
-	}
-
-	// log it to the writer
-	private void logStatsRecord(SessionRecord rec) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	private void updateRecordOnDisconnection(SessionRecord rec, SharingPeer peer) {
-		// TODO - implement !!!!!!!!!
-		
+		ps.setCurrentSessionRecord(null);
 	}
 
 	@Override
 	public void handleIOException(SharingPeer peer, IOException ioe) {  /* ignore */ }
 
 	@Override
+	// update the zero record for all relevant peers
 	public void handleAnnounceResponse(Map<String, BEValue> answer) {
-		/* TODO - update the zero record for all relevant peers !!!!!!!!!
-		use similar method in Client
-		for each peer do:
-		first check if contains. if not - create new zero rec anyway, in case this peer changed ID
-		Map<Integer, StatsLoggerReccord> peerSessions = sessionsMap.get(peer.hasPeerId() 
-				? peer.getHexPeerId() : peer.getHostIdentifier());
+		try {
+			if (!answer.containsKey("peers")) {
+				// No peers returned by the tracker. Apparently we're alone on this one for now.
+				return;
+			}	
+			if (answer.containsKey("complete"))
+				this.numSeeds = answer.get("complete").getInt();
+			if (answer.containsKey("incomplete"))
+				this.numLeeches = answer.get("incomplete").getInt();						
+
+			try { 
+				List<BEValue> peers = answer.get("peers").getList();
+				for (BEValue peerInfo : peers) {
+					Map<String, BEValue> info = peerInfo.getMap();
+
+					try {
+						byte[] peerId = info.get("peer id").getBytes();
+						String ip = new String(info.get("ip").getBytes(),
+								Torrent.BYTE_ENCODING);
+						int port = info.get("port").getInt();
+						this.upsertZeroRecord(peerId, ip, port);
+					} catch (NullPointerException npe) {
+						throw new ParseException("Missing field from peer " +
+								"information in tracker response!", 0);
+					}
+				}
+			} catch (InvalidBEncodingException ibee) {
+				byte[] data = answer.get("peers").getBytes();
+				int nPeers = data.length / 6;
+				if (data.length % 6 != 0) {
+					throw new InvalidBEncodingException("Invalid peers " +
+							"binary information string!");
+				}
+
+				ByteBuffer peers = ByteBuffer.wrap(data);
+
+				for (int i=0; i < nPeers ; i++) {
+					byte[] ipBytes = new byte[4];
+					peers.get(ipBytes);
+					String ip = InetAddress.getByAddress(ipBytes)
+						.getHostAddress();
+					int port = (0xFF & (int)peers.get()) << 8
+						| (0xFF & (int)peers.get());
+					this.upsertZeroRecord(null, ip, port);
+				}
+			}
+		} catch (UnknownHostException uhe) {
+			logger.warn("Invalid compact tracker response!", uhe);
+		} catch (ParseException pe) {
+			logger.warn("Invalid tracker response!", pe);
+		} catch (InvalidBEncodingException ibee) {
+			logger.warn("Invalid tracker response!", ibee);
+		} catch (UnsupportedEncodingException uee) {
+			logger.error("StatsLogger error: {}", uee.getMessage(), uee);
+		}
+	}					
 				
-		StatsLoggerReccord rec0 = peerSessions.get(0);
-		
-		updateZeroRecordOnDisconnection(rec0, peer);
-		
-		*/ 
-		
+	private void upsertZeroRecord(byte[] initialPeerId, String ip, int port) {
+		Peer basePeer = generatePeerLikeClientDoes(initialPeerId, ip, port);
+		String peerId = basePeer.hasPeerId() ? basePeer.getHexPeerId() : basePeer.getHostIdentifier();		
+		if (sessionsMap.containsKey(peerId)){							
+			SessionRecord rec0 = sessionsMap.get(peerId).getZeroSessionRecord();			
+			if (rec0 == null) { // need to init the zero record
+				rec0 = createZeroRecord(ip, port, basePeer);
+				sessionsMap.get(peerId).setZeroSessionRecord(rec0);
+			} else { // just update the zero record				
+				rec0.lastSeenByTracker = new Date();
+				rec0.lastNumOfLeeches = this.getNumLeechInTorrent();
+				rec0.lastNumOfSeeds = this.getNumSeedsInTorrent();	
+			}			
+		} else { // first time we encounter this peerId
+			SessionRecord rec0 = createZeroRecord(ip, port, basePeer);
+			PeerStats ps = new PeerStats();
+			ps.setZeroSessionRecord(rec0);
+			ps.setBasePeer(basePeer);
+		}				
+	}
+	
+	private Peer generatePeerLikeClientDoes(byte[] initialPeerId,
+			String ip, int port) {
+		Peer peer = new Peer(ip, port,
+				(initialPeerId != null ? ByteBuffer.wrap(initialPeerId) : (ByteBuffer)null));
+		return peer;
 	}
 
-	@Override
-	public void run() {
-		logger.info("Starting statsLogger thread for " +
-				torrent.getName() + " to " +
-				torrent.getAnnounceUrl() +
-				"with CrawlerID " + this.crawlerPeerID + "...");
+	private SessionRecord createZeroRecord(String ip, int port, Peer basePeer) {
+		SessionRecord rec0 = new SessionRecord();		
+		rec0.crawlerPeerID = this.crawlerPeerID;
+		rec0.initialNumOfLeeches = this.getNumLeechInTorrent();
+		rec0.initialNumOfSeeds = this.getNumSeedsInTorrent();
+		rec0.peerIdHex = basePeer.getHexPeerId();
+		rec0.peerIdStr = basePeer.getPeerIdStr();
+		rec0.peerIP = ip;
+		rec0.peerPort = port;
+		rec0.sessionSeqNum = 0;
+		rec0.lastSeenByTracker = new Date();
+		rec0.lastNumOfLeeches = this.getNumLeechInTorrent();
+		rec0.lastNumOfSeeds = this.getNumSeedsInTorrent();
 		
-		
-		sessionsMap = new ConcurrentHashMap<String, Map<Integer, SessionRecord>>();
-		
-		int numSeedsInTorrent = getNumSeedsInTorrent();
-		int numLeechInTorrent = getNumLeechInTorrent();
-		
-		for (SharingPeer peer : this.connectedPeers.values()) {
-			// init zero record (belongs to info received from tracker)
-			SessionRecord rec0 = new SessionRecord();
-			initZeroRecord(rec0, peer);
-			
-			Map<Integer, SessionRecord> peerSessions = new HashMap<Integer, SessionRecord>();
-			peerSessions.put(0, rec0);
-						
-			// init first record
-			SessionRecord rec1 = createNewRecord(numSeedsInTorrent,
-					numLeechInTorrent, peer);
-			
-			peerSessions.put(1, rec1);
-			this.sessionsMap.put(peer.getHexPeerId(), peerSessions);		
-		}
-		
-		this.sleepIntervalMiliSecs = 3;
-		
-		while (!this.stop) {		
-			
-			// TODO - update sessionsMap every interval?
-						
-			try {
-				logger.trace("going to sleep for " + this.sleepIntervalMiliSecs +
-					   	" seconds.");
-				
-				Thread.sleep(this.sleepIntervalMiliSecs * 1000);
-			} catch (InterruptedException ie) {
-				// Ignore
-			}
-		}
-		
-		// TODO  - consider flushing all data to Writer ??
-		
+		return rec0;
 	}
 
 	private SessionRecord createNewRecord(int numSeedsInTorrent,
-			int numLeechInTorrent, SharingPeer peer) {
+			int numLeechInTorrent, SharingPeer peer, int sessionSeqNum) {
 		SessionRecord rec = new SessionRecord();
 		rec.lastDownloadRate = peer.getDLRate();
 		rec.completionRate = peer.getAvailablePieces().cardinality() / torrent.getPieceCount();
@@ -199,25 +282,34 @@ public class StatsLogger implements Runnable,
 		rec.lastNumOfLeeches = numLeechInTorrent;
 		rec.lastNumOfSeeds = numSeedsInTorrent;
 		rec.lastBitfield = peer.getAvailablePieces();
-		rec.peerID = peer.getPeerIdStr();
+		// dont confuse getPeerIdStr (for output in log records) and peer.getHexPeerId (which is for MAPs)
+		rec.peerIdHex = peer.getHexPeerId();
+		rec.peerIdStr = peer.getPeerIdStr();
 		rec.peerIP = peer.getIp();
 		rec.peerPort = peer.getPort();
-		rec.sessionSeqNum = 1;
+		rec.sessionSeqNum = sessionSeqNum;
 		rec.startTime = new Date();
 		rec.lastSeen = new Date();
 		return rec;
 	}
 
+	private void updateRecordOnDisconnection(SessionRecord rec, SharingPeer peer, 
+			boolean isDisconnectedByCrawler, int numSeedsInTorrent, int numLeechInTorrent) {
+		rec.completionRate = peer.getAvailablePieces().cardinality() / torrent.getPieceCount();
+		rec.isDisconnectedByCrawler = isDisconnectedByCrawler;
+		rec.lastBitfield = peer.getAvailablePieces();
+		rec.lastDownloadRate = peer.getDLRate();
+		rec.lastNumOfLeeches = numLeechInTorrent;
+		rec.lastNumOfSeeds = numSeedsInTorrent;
+		rec.lastSeen = new Date();		
+	}
+	
 	private int getNumLeechInTorrent() {
-		// TODO Auto-generated method stub
-		// TODO - implement!!!!
-		return 0;
+		return this.numLeeches;
 	}
 
 	private int getNumSeedsInTorrent() {
-		// TODO Auto-generated method stub
-		// TODO - implement!!!!
-		return 0;
+		return this.numSeeds;
 	}
 
 	@Override
@@ -231,55 +323,29 @@ public class StatsLogger implements Runnable,
 			throws IOException { /* ignore */ }
 
 	public void addNewConnectedPeer(SharingPeer peer) {
+		
 		int numSeedsInTorrent = getNumSeedsInTorrent();
 		int numLeechInTorrent = getNumLeechInTorrent();
 		
-		if (sessionsMap.containsKey(peer.getHexPeerId()))
-		{
-			Map<Integer, SessionRecord> peerSessions = sessionsMap.get(peer.getHexPeerId());
-			SessionRecord rec0 = peerSessions.get(0);
-			updateZeroRecordOnConnection(rec0, peer);
-		} 
-		else
-		{
-			SessionRecord rec0 = new SessionRecord();
-			initZeroRecord(rec0, peer);
-			
-			Map<Integer, SessionRecord> peerSessions = new HashMap<Integer, SessionRecord>();
-			peerSessions.put(0, rec0);
-						
-			// init first record
-			SessionRecord rec1 = createNewRecord(numSeedsInTorrent,
-					numLeechInTorrent, peer);
-			
-			peerSessions.put(1, rec1);
-			this.sessionsMap.put(peer.getHexPeerId(), peerSessions);
-			
+		if (sessionsMap.containsKey(peer.getHexPeerId())) {  
+			// means we already have some record of this peer - a regular record and/or a zero record	
+			PeerStats ps = sessionsMap.get(peer.getHexPeerId());
+			ps.setConnected(true);
+			ps.setCurrentSessionNum(ps.getCurrentSessionNum() + 1);
+			SessionRecord newRecord = createNewRecord(numSeedsInTorrent, numLeechInTorrent, peer,
+					ps.getCurrentSessionNum());
+			ps.setCurrentSessionRecord(newRecord);
+			ps.setSharingPeer(peer);  // this line is necessary in case we only had a zero record for this peerId			
+		} else {  // means we don't have any record for this peer					
+			// init peersStats and first record
+			PeerStats ps = new PeerStats();
+			ps.setConnected(true);
+			ps.setCurrentSessionNum(1);
+			ps.setSharingPeer(peer);
+			SessionRecord rec1 = createNewRecord(numSeedsInTorrent, numLeechInTorrent, peer, 1);
+			ps.setCurrentSessionRecord(rec1);
+			this.sessionsMap.put(peer.getHexPeerId(), ps);
 		}
 		
-	}
-
-	private void initZeroRecord(SessionRecord rec0, SharingPeer peer) {
-		rec0.lastSeenByTracker = new Date();
-		rec0.sessionSeqNum = 0;
-		rec0.peerID = peer.getPeerIdStr();
-		rec0.peerIP = peer.getIp();
-		rec0.peerPort = peer.getPort();
-	}
-
-	private void updateZeroRecordOnConnection(SessionRecord rec0,
-			SharingPeer peer) {
-		// TODO Auto-generated method stub
-		// TODO - in general, dont confuse getPeerIdStr (for log records) 
-		// and peer.getHexPeerId (which is for MAPs)
-		
-	}
-
-	// consider if there should be a difference between this method and the one above
-	private void updateZeroRecordOnDisconnection(SessionRecord rec0,
-			SharingPeer peer) {
-		// TODO Auto-generated method stub
-		
 	}	
-
 }
