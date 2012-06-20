c8f59f787b2103298ab09f594282cb551a2603ba
asafshmir <asafshmir@99d24f90-0284-dfdf-c06c-bb8da34ac033>
2012-06-20 04:21:30 +0000

    update sql scripts

--- TIMCrawler/db/churn.sql
+++ TIMCrawler/db/churn.sql
@@ -1,4 +1,6 @@
-select *, timediff(t.end_time,s.last_seen), t.end_time, s.last_seen, max(s.last_seen)
+select *
 from tim.sessions s, tim.tests t 
-where s.fk_test_id=t.id and completion_rate<1 and timediff(t.end_time,s.last_seen) > '00:20:00' and t.id=51
+where s.fk_test_id=t.id and session_num>0 and
+timediff(t.end_time,s.last_seen) > '00:30:00' and t.id=96
 group by s.peer_id
+limit 10000
--- TIMCrawler/db/sessions.sql
+++ TIMCrawler/db/sessions.sql
@@ -1,2 +1,2 @@
 SELECT *,timediff(last_seen,start_time) FROM tim.sessions
-where timediff(last_seen,start_time) > '00:01:00' and fk_test_id=53;
+where timediff(last_seen,start_time) > '00:00:00' and fk_test_id=(select max(id) from tim.tests)
