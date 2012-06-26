3557c25c3bde170a15e77bf4e030c0ddd998e8d1
asafshmir <asafshmir@99d24f90-0284-dfdf-c06c-bb8da34ac033>
2012-06-26 08:54:59 +0000

    avoid blocking by read() from InputStream

--- trunk/TIMCrawler/src/com/turn/ttorrent/client/ConnectionHandler.java
+++ trunk/TIMCrawler/src/com/turn/ttorrent/client/ConnectionHandler.java
@@ -326,12 +326,30 @@ public class ConnectionHandler implements Runnable {
 	private Handshake validateHandshake(Socket socket, byte[] peerId)
 		throws IOException, ParseException {
 		InputStream is = socket.getInputStream();
-
-		// Read the handshake from the wire
-		int pstrlen = is.read();
-		byte[] data = new byte[Handshake.BASE_HANDSHAKE_LENGTH + pstrlen];
-		data[0] = (byte)pstrlen;
-		is.read(data, 1, data.length-1);
+		
+		byte[] data = null;
+		int pstrlen = 0;
+		
+		int nTries = 20;
+		Outer: while (nTries > 0) {
+			while (is.available() > 0) {
+				// Read the handshake from the wire
+				pstrlen = is.read();
+				if (pstrlen < 0)
+					throw new IOException("Unable to read");
+				data = new byte[Handshake.BASE_HANDSHAKE_LENGTH + pstrlen];
+				data[0] = (byte)pstrlen;
+				is.read(data, 1, data.length-1);
+				break Outer;
+			}
+			try {
+				Thread.sleep(1000);
+			} catch (Exception ee) { /* ignore */ }
+			nTries--;
+		}
+		
+		if (nTries == 0)
+			throw new IOException("Unable to read");
 
 		// Parse and check the handshake
 		Handshake hs = Handshake.parse(ByteBuffer.wrap(data));
