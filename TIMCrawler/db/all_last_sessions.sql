478913601eeed4d17324addf778858851c4f87a7
asafshmir <asafshmir@99d24f90-0284-dfdf-c06c-bb8da34ac033>
2012-07-27 11:12:13 +0000

    update sql scripts

--- TIMCrawler/db/all_last_sessions.sql
+++ TIMCrawler/db/all_last_sessions.sql
@@ -1,8 +1,8 @@
 select * from tim.tests t, tim.sessions s,
 (select peer_id,fk_test_id,peer_ip,max(session_num) s_max
-from tim.sessions where session_num>0 and fk_test_id=93
+from tim.sessions where session_num>0 and fk_test_id=178
 group by peer_id) tmp
 where s.fk_test_id=t.id and s.peer_id=tmp.peer_id and s.fk_test_id=tmp.fk_test_id
 and s.peer_ip=tmp.peer_ip and s.session_num=tmp.s_max
-and (timediff(now(),s.last_seen) > '00:30:00' or completion_rate>=0.95)
+and (timediff(t.end_time,s.last_seen) > '00:30:00' or completion_rate>=0.95)
 limit 10000
